288fe2ea6c42d75b2694978a64533c3e012d7d9a
Teodora Uzunova <tedyuzunova@gmail.com>
2022-03-22 17:10:04 +0200

    add Ex_22

---
+++ 22_Exam Preparation/RealDeal_Skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -0,0 +1,34 @@
+package softuni.exam.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import softuni.exam.util.ValidationUtil;
+
+
+@Configuration
+public class ApplicationBeanConfiguration {
+
+    //ToDo
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setPrettyPrinting()
+                .create();
+    }
+
+//    @Bean
+//    public ValidationUtil validationUtil() {
+//        return null;
+//    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
